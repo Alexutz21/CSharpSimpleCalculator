a7972743b2722685767f5701a8415f8f503878cf
Alexandru Constantin Lungu <70633367+Alexutz21@users.noreply.github.com>
2021-12-27 15:22:39 +0200

    Add files via upload

---
+++ simpleCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace simpleCalculator
+{
+    class Program
+    {
+        private static char userInput;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                calculator.runCalculator();
+                Console.WriteLine("Do you like to continue ? Press y or n");
+                char userInput = Convert.ToChar(Console.ReadLine());
+           
+           if (userInput == 'y')
+            {
+                calculator.runCalculator();
+            }
+            else if (userInput == 'n')
+                {
+                    Console.WriteLine("Thank you!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ simpleCalculator/calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+    public class calculator
+    {
+        public static void ourCalculator()
+        {
+        int a, b;
+        int sum;
+        Console.WriteLine("SIMPLE CALCULATOR");
+            Console.WriteLine();
+            
+            Console.Write("Please enter a number and press enter: ");
+            
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Perfect,\nnow enter another number and press enter: ");
+                b = Convert.ToInt32(Console.ReadLine()); Console.WriteLine();
+            Console.Write("What operation do you want to perform: ");
+                char operation = Convert.ToChar(Console.ReadLine()); Console.WriteLine();
+            if (operation == '+')
+            {
+                Console.WriteLine("You chose addition of the numbers");
+                sum = (a + b);
+                Console.WriteLine($"Number {a} + {b} = {sum}");
+                Console.WriteLine();
+            }
+            else if (operation == '-')
+            {
+                sum = (a - b);
+                Console.WriteLine("You chose subtract the numbers ");
+                Console.WriteLine($"Number {a} - {b} = {sum}"); Console.WriteLine();
+            }
+            else if (operation == '*')
+            {
+                int sumM = (a * b);
+                Console.WriteLine("You have chosen to multiply the numbers");
+                Console.WriteLine($"Number {a} * {b} = {sumM}"); Console.WriteLine();
+            }
+            else if (operation == '/')
+            {
+                double sumD = (a / b);
+                Console.WriteLine("You have chosen to divide the numbers");
+                Console.WriteLine($"Number {a} / {b} = {sumD}"); Console.WriteLine();
+            }
+        }
+        public static void runCalculator()
+        {
+            ourCalculator();
+        }
+        
+    }
+}
